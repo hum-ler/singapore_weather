0cf4dda03e7fcc6f4519f1f21754a620288ae3b1
Hum Ler <hum.ler@outlook.com>
2021-04-07 14:08:36 +0800

    Add comments to WeatherModel

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -6,42 +6,61 @@ import 'reading.dart';
 
 /// Collection of weather data to be maintained in state.
 class WeatherModel extends ChangeNotifier {
+  /// The timestamp of the last modification.
   DateTime? _timestamp;
 
+  /// The temperature reading.
   Reading? _temperature;
 
+  /// The rainfall reading.
   Reading? _rain;
 
+  /// The relative humidity reading.
   Reading? _humidity;
 
+  /// The wind speed reading.
   Reading? _windSpeed;
 
+  /// The wind direction reading.
   Reading? _windDirection;
 
+  /// The PM2.5 reading.
   Reading? _pm2_5;
 
+  /// The weather condition.
   Condition? _condition;
 
+  /// The weather forecast.
   Iterable<Forecast>? _forecast;
 
+  /// The timestamp of the last modification.
   DateTime? get timestamp => _timestamp;
 
+  /// The temperature reading.
   Reading? get temperature => _temperature;
 
+  /// The rainfall reading.
   Reading? get rain => _rain;
 
+  /// The relative humidity reading.
   Reading? get humidity => _humidity;
 
+  /// The wind speed reading.
   Reading? get windSpeed => _windSpeed;
 
+  /// The wind direction reading.
   Reading? get windDirection => _windDirection;
 
+  /// The PM2.5 reading.
   Reading? get pm2_5 => _pm2_5;
 
+  /// The weather condition.
   Condition? get condition => _condition;
 
+  /// The weather forecast.
   Iterable<Forecast>? get forecast => _forecast;
 
+  /// Resets all fields to null.
   void clear() {
     _timestamp = null;
     _temperature = null;
@@ -56,6 +75,7 @@ class WeatherModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Sets all the fields at once.
   void refresh({
     required DateTime timestamp,
     required Reading? temperature,
@@ -80,6 +100,10 @@ class WeatherModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Updates only the given fields.
+  ///
+  /// If a given argument is null, the underlying field will not be updated. To
+  /// set a field to null, use [clear()] or [refresh()] instead.
   void update({
     required DateTime timestamp,
     Reading? temperature,
