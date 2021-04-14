b6f71ddc5f63d9de0e8d9d3f68bb9b967ac3b0ba
Hum Ler <hum.ler@outlook.com>
2021-04-15 05:44:17 +0800

    Use row of columns instead of grid view

--- lib/screens/island.dart
+++ lib/screens/island.dart
@@ -23,6 +23,7 @@ class Island extends StatelessWidget {
           // The map image.
           Container(
             height: double.infinity,
+            width: double.infinity,
             child: Hero(
               tag: K.islandImageTag,
               child: Image(
@@ -37,46 +38,61 @@ class Island extends StatelessWidget {
           // The forecast icons.
           Center(
             child: Consumer<WeatherModel>(
-              builder: (context, data, child) => GridView.count(
-                crossAxisCount: 3,
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
+              builder: (context, data, child) => Row(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Container(),
-                  if (data.forecast != null &&
-                      data.forecast![Sources.north] != null)
-                    ForecastRow(data.forecast![Sources.north]!)
-                  else
-                    Container(),
-                  Container(),
-                  if (data.forecast != null &&
-                      data.forecast![Sources.west] != null)
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 24.0),
-                      child: ForecastRow(data.forecast![Sources.west]!),
-                    )
-                  else
-                    Container(),
-                  if (data.forecast != null &&
-                      data.forecast![Sources.central] != null)
-                    ForecastRow(data.forecast![Sources.central]!)
-                  else
-                    Container(),
-                  if (data.forecast != null &&
-                      data.forecast![Sources.east] != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 24.0),
-                      child: ForecastRow(data.forecast![Sources.east]!),
-                    )
-                  else
-                    Container(),
-                  Container(),
-                  if (data.forecast != null &&
-                      data.forecast![Sources.south] != null)
-                    ForecastRow(data.forecast![Sources.south]!)
-                  else
-                    Container(),
-                  Container(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(),
+                      if (data.forecast != null &&
+                          data.forecast![Sources.west] != null)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 24.0),
+                          child: ForecastRow(data.forecast![Sources.west]!),
+                        )
+                      else
+                        Container(),
+                      Container(),
+                    ],
+                  ),
+                  SizedBox(width: 8.0),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      if (data.forecast != null &&
+                          data.forecast![Sources.north] != null)
+                        ForecastRow(data.forecast![Sources.north]!)
+                      else
+                        Container(),
+                      if (data.forecast != null &&
+                          data.forecast![Sources.central] != null)
+                        ForecastRow(data.forecast![Sources.central]!)
+                      else
+                        Container(),
+                      if (data.forecast != null &&
+                          data.forecast![Sources.south] != null)
+                        ForecastRow(data.forecast![Sources.south]!)
+                      else
+                        Container(),
+                    ],
+                  ),
+                  SizedBox(width: 8.0),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(),
+                      if (data.forecast != null &&
+                          data.forecast![Sources.west] != null)
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24.0),
+                          child: ForecastRow(data.forecast![Sources.west]!),
+                        )
+                      else
+                        Container(),
+                      Container(),
+                    ],
+                  ),
                 ],
               ),
             ),
